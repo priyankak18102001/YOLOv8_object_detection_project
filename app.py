7b3d4a20962c706969e07717db430cd02da33b35
Priyanka Kumawat <priyankakumawat227@gmail.com>
2026-03-04 18:22:27 +0530

    Fix Streamlit crash for long videos

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ elif page == "Detection":
                     save=True,
                     conf=confidence,
                     imgsz=320,
-                    vid_stride=10,
+                    vid_stride=2,
                     stream=True
                ))
             
