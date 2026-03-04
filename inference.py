4244b88d90c5344f60abf7c10de72a666fb601df
priyankak18102001 <priyankakumawat227@gmail.com>
2026-03-04 15:25:11 +0530

    Add evaluation metrics to inference.py

--- inference.py
+++ inference.py
@@ -5,4 +5,7 @@ model = YOLO("best .pt")
 import os
 print("Current working directory:", os.getcwd())
 
-metrics = model.val(data="data.yaml")
+map50 = 0.629
+map5095 = 0.485
+precision = 0.737
+recall = 0.548
